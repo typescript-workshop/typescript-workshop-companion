7ecb32ce5cbc4e4bc5767bf56fdea9a163551773
Johan ROUVE <johan.rouve@gmail.com>
2025-09-18 15:11:24 +0200

    Use recommended setting for GH page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
 
   // Set the production url of your site here
   url: "https://typescript-workshop.github.io",
+  trailingSlash: true,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/typescript-workshop-companion/",
